f3c82f7b9eb23ff215d29969c599577051f9c507
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2020-08-13 16:16:11 +0100

    TRUS-2136| Added pages

---
+++ app/controllers/register/trustees/individual/IDCardDetailsController.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.trustees.individual
+
+import controllers.actions._
+import controllers.actions.register.{DraftIdRetrievalActionProvider, RegistrationDataRequiredAction, RegistrationIdentifierAction}
+import controllers.filters.IndexActionFilterProvider
+import forms.trustees.PassportOrIdCardFormProvider
+import javax.inject.Inject
+import navigation.Navigator
+import pages.register.trustees.individual.{IDCardDetailsPage, TrusteesNamePage}
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.RegistrationsRepository
+import sections.Trustees
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import utils.countryOptions.CountryOptions
+import views.html.register.trustees.individual.IDCardDetailsView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class IDCardDetailsController @Inject()(
+                                         override val messagesApi: MessagesApi,
+                                         registrationsRepository: RegistrationsRepository,
+                                         navigator: Navigator,
+                                         identify: RegistrationIdentifierAction,
+                                         getData: DraftIdRetrievalActionProvider,
+                                         validateIndex: IndexActionFilterProvider,
+                                         requireData: RegistrationDataRequiredAction,
+                                         requiredAnswer: RequiredAnswerActionProvider,
+                                         formProvider: PassportOrIdCardFormProvider,
+                                         val controllerComponents: MessagesControllerComponents,
+                                         view: IDCardDetailsView,
+                                         val countryOptions: CountryOptions
+                                       )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private val form = formProvider("iDCardDetails")
+
+  private def actions(index: Int, draftId: String) =
+    identify andThen
+      getData(draftId) andThen
+      requireData andThen
+      validateIndex(index, Trustees) andThen
+      requiredAnswer(RequiredAnswer(TrusteesNamePage(index), routes.TrusteesNameController.onPageLoad(index, draftId)))
+
+  def onPageLoad(index: Int, draftId: String): Action[AnyContent] = actions(index, draftId) {
+    implicit request =>
+
+      val trusteeName = request.userAnswers.get(TrusteesNamePage(index)).get.toString
+
+      val preparedForm = request.userAnswers.get(IDCardDetailsPage(index)) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, countryOptions.options, draftId, index, trusteeName))
+  }
+
+  def onSubmit(index: Int, draftId: String): Action[AnyContent] = actions(index, draftId).async {
+    implicit request =>
+
+      val trusteeName = request.userAnswers.get(TrusteesNamePage(index)).get.toString
+
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          Future.successful(BadRequest(view(formWithErrors, countryOptions.options, draftId, index, trusteeName))),
+
+        value => {
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(IDCardDetailsPage(index), value))
+            _              <- registrationsRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(IDCardDetailsPage(index), draftId, updatedAnswers))
+        }
+      )
+  }
+}
