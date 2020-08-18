c2098f11788a2789d83c927a96e678a5ae30a271
Darryl Murphy <darrylmurphy@users.noreply.github.com>
2020-08-18 17:41:24 +0100

    Test for trustee ind check details

---
+++ test/controllers/register/trustees/individual/CheckDetailsControllerSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.trustees.individual
+
+import base.SpecBase
+import models.UserAnswers
+import models.core.pages.FullName
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.mockito.MockitoSugar
+import pages.register.trustees.individual.NamePage
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import utils.print.TrusteeIndividualPrintHelper
+import views.html.register.trustees.individual.CheckDetailsView
+
+class CheckDetailsControllerSpec extends SpecBase with MockitoSugar with ScalaFutures {
+
+  private val index: Int = 0
+  private val name = FullName("Test", None, "Name")
+
+  private lazy val checkDetailsRoute = routes.CheckDetailsController.onPageLoad(index, fakeDraftId).url
+
+  override val emptyUserAnswers: UserAnswers = super.emptyUserAnswers
+    .set(NamePage(index), name).success.value
+
+  "CheckDetails Controller" must {
+
+    "return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      val request = FakeRequest(GET, checkDetailsRoute)
+
+      val result = route(application, request).value
+
+      val view = application.injector.instanceOf[CheckDetailsView]
+      val printHelper = application.injector.instanceOf[TrusteeIndividualPrintHelper]
+      val answerSection = printHelper.checkDetailsSection(emptyUserAnswers, name.toString, index, fakeDraftId)
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(answerSection, index, fakeDraftId)(fakeRequest, messages).toString
+    }
+
+  }
+}
