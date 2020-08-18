f919880d0bb366d2325fc9e00573542ca9c396d1
Darryl Murphy <darrylmurphy@users.noreply.github.com>
2020-08-18 14:13:19 +0100

    Move trustee ind to routes

---
+++ app/controllers/register/trustees/individual/InternationalAddressController.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register.trustees.individual
+
+import config.annotations.TrusteeOrganisation
+import controllers.actions._
+import controllers.actions.register.trustees.organisation.NameRequiredActionImpl
+import forms.InternationalAddressFormProvider
+import javax.inject.Inject
+import navigation.Navigator
+import pages.register.trustees.organisation.InternationalAddressPage
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.RegistrationsRepository
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import utils.countryOptions.CountryOptionsNonUK
+import views.html.register.trustees.organisation.InternationalAddressView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class InternationalAddressController @Inject()(
+                                                override val messagesApi: MessagesApi,
+                                                registrationsRepository: RegistrationsRepository,
+                                                @TrusteeOrganisation navigator: Navigator,
+                                                standardActionSets: StandardActionSets,
+                                                nameAction: NameRequiredActionImpl,
+                                                formProvider: InternationalAddressFormProvider,
+                                                val controllerComponents: MessagesControllerComponents,
+                                                view: InternationalAddressView,
+                                                val countryOptions: CountryOptionsNonUK
+                                              )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private val form = formProvider()
+
+  private def actions(index: Int, draftId: String) =
+    standardActionSets.identifiedUserWithData(draftId) andThen nameAction(index)
+
+  def onPageLoad(index: Int, draftId: String): Action[AnyContent] = actions(index, draftId) {
+    implicit request =>
+
+      val preparedForm = request.userAnswers.get(InternationalAddressPage(index)) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, countryOptions.options, index, draftId, request.trusteeName))
+  }
+
+  def onSubmit(index: Int, draftId: String): Action[AnyContent] = actions(index, draftId).async {
+    implicit request =>
+
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) =>
+          Future.successful(BadRequest(view(formWithErrors, countryOptions.options, index, draftId, request.trusteeName))),
+
+        value => {
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(InternationalAddressPage(index), value))
+            _              <- registrationsRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(InternationalAddressPage(index), draftId, updatedAnswers))
+        }
+      )
+  }
+}
