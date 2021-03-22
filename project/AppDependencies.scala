263da7df165dc5a34b7478ce3c930d0be8922873
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2021-03-22 20:07:03 +0000

    PlayUpgrade| To 4.1

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,13 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.6.0",
     "uk.gov.hmrc"       %% "govuk-template"                 % "5.61.0-play-27",
     "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-27",
     "uk.gov.hmrc"       %% "play-ui"                        % "9.0.0-play-27",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.5.0-play-27",
     "uk.gov.hmrc"       %% "domain"                         % "5.10.0-play-27",
     "uk.gov.hmrc"       %% "emailaddress"                   % "3.5.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "2.25.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "4.1.0",
     "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
     "uk.gov.hmrc"       %% "play-language"                  % "4.10.0-play-27"
  )
