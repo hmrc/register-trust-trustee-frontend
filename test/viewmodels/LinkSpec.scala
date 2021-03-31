a5f5868e93dfa3a310f9dea2f160a8a5f466a73d
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-03-31 08:30:11 +0100

    Attempting to fix axe issues that have come out of testing.

--- test/viewmodels/LinkSpec.scala
+++ test/viewmodels/LinkSpec.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,16 +12,31 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@import viewmodels.Link
+package viewmodels
 
-@(link: Link)(implicit messages: Messages)
+import base.SpecBase
 
-<li class="app-link-list__item" id="link-list__item--@{link.cssText}">
-    <span class="app-link-link">
-        <a class="app-link-list__link-name" id="link-list__link--@{link.cssText}" href="@{link.url}">
-            @messages(link.text)
-        </a>
-    </span>
-</li>
+class LinkSpec extends SpecBase {
+
+  "Link" when {
+
+    val url = "url"
+
+    ".cssText" must {
+      "be in lower case and replace spaces with hyphens" when {
+
+        "Passport and address" in {
+          val link = Link("Passport and address", url)
+          link.cssText mustBe "passport-and-address"
+        }
+
+        "ID and address" in {
+          val link = Link("ID and address", url)
+          link.cssText mustBe "id-and-address"
+        }
+      }
+    }
+  }
+}
