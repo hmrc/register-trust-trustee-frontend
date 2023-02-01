396840a80dc8fea3ca830adc5ae7e052117b96a7
Victor Osipov <osipovvictor1994@gmail.com>
2023-02-01 11:31:36 +0000

    DDCE-3332 - Update deps, + sbt, + plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ object AppDependencies {
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "3.22.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "6.3.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.1.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.12.0-play-28",
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "7.11.0",
-    "uk.gov.hmrc"             %% "emailaddress"                   % "3.6.0"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "7.13.0",
+    "uk.gov.hmrc"             %% "emailaddress"                   % "3.7.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
@@ -17,7 +17,7 @@ object AppDependencies {
     "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "org.jsoup"                   %  "jsoup"                    % "1.15.3",
-    "org.scalatest"               %% "scalatest"                % "3.2.14",
+    "org.scalatest"               %% "scalatest"                % "3.2.15",
     "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.12",
     "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
