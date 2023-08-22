5b63f00b3195f171bd4f0e8f78949e7b97df092d
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-08-22 12:44:49 +0100

    DDCE-4530: Changes as part of sm2 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.19.0"
+  val bootstrapVersion = "7.21.0"
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.14.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % bootstrapVersion,
