464453b47137b7e43cfa6e6584fb84cfe8b02a52
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-09-04 09:41:54 +0100

    DDCE-4530: Changes as part of sm2 upgrade (#206)
    * DDCE-4530: Changes as part of sm2 upgrade
    
    * DDCE-4530: PR feedback and a few bonus bits

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,12 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.19.0"
+  val bootstrapVersion = "7.21.0"
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.14.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % bootstrapVersion,
@@ -17,9 +17,9 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
     "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
-    "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.0.1",
+    "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
