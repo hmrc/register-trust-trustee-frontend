b0acfe4e15e2a56cd815ba961b33de9de17978c5
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2024-02-29 08:47:38 +0000

    DDCE-5380: Update HMRC logo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val bootstrapVersion = "7.23.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.29.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-28"     % "8.5.0",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % bootstrapVersion,
@@ -15,9 +15,9 @@ object AppDependencies {
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
     "org.jsoup"                   %  "jsoup"                    % "1.17.2",
-    "org.scalatest"               %% "scalatest"                % "3.2.17",
+    "org.scalatest"               %% "scalatest"                % "3.2.18",
     "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.30",
-    "org.wiremock"                %  "wiremock-standalone"      % "3.3.1",
+    "org.wiremock"                % "wiremock-standalone"       % "3.4.2",
     "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.17.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
