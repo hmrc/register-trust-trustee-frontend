5a3a63898e8f89d3899575f6e636e983a24aab25
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2025-02-11 13:32:55 +0000

    DDCE-5355 amend regex comment

--- app/forms/Validation.scala
+++ app/forms/Validation.scala
@@ -22,9 +22,9 @@ object Validation {
   val postcodeRegex = """^[a-zA-Z]{1,2}[0-9][0-9a-zA-Z]?\s?[0-9][a-zA-Z]{2}$"""
 
   /** This is the actual regex for an individual's name:
-   * "^(?=.{1,99}$)([A-Z]([-'. ]{0,1}[A-Za-z ]+)*[A-Za-z]?)$"
-   * It has been amended to be more permissive, allowing errors to be shown in the sequence:
-   * 1 - invalid characters, 2 - over max length, 3 - no starting capital letter
+   * `"^(?=.{1,99}$)([A-Z]([-'. ]{0,1}[A-Za-z ]+)*[A-Za-z]?)$"`
+   * It has been amended to be more permissive, though other constraints catch the removed regex patterns.
+   * This allows errors to be shown in the sequence: 1 - invalid characters, 2 - over max length, 3 - no starting capital letter
    */
   val nameRegex = "^(?=.{1,99}$)([A-Za-z]([-'. ]{0,1}[A-Za-z ]+)*[A-Za-z]?)$"
 
