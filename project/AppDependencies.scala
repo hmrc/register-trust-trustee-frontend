af1f7dacbbd09bbf5564eb83dba9dcaec1196cf8
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2026-01-27 22:39:19 +0000

    DDCE-7777: Deps updated.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   private val playBootstrapVersion = "10.5.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.18.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.27.0",
     "uk.gov.hmrc"         %% "domain-play-30"                         % "11.0.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % playBootstrapVersion
