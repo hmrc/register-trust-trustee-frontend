4f0043bb6ec6c3c756ee1af24bf6dfb329d4d770
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2026-01-27 10:13:02 +0000

    DDCE-7777: [Trusts]: Migrate all Trusts Frontends to use the new gov.uk Service Navigation Component (19 - 22)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val playBootstrapVersion = "10.3.0"
+  private val playBootstrapVersion = "10.5.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.18.0",
     "uk.gov.hmrc"         %% "domain-play-30"                         % "11.0.0",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % playBootstrapVersion
   )
 
