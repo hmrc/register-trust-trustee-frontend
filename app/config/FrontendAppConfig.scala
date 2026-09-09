21c6c90b8b953ba3989f1c7a4bf9005bfcfa2f55
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2026-09-09 14:02:36 +0100

    DDCE-9176 fmt

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -34,7 +34,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   lazy val loginUrl: String         = configuration.get[String]("urls.login")
   lazy val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
   lazy val logoutUrl: String        = s"${configuration.get[String]("urls.logout")}?useServiceNavigation"
-  val appName: String = configuration.get[String]("appName")
+  val appName: String               = configuration.get[String]("appName")
 
   lazy val logoutAudit: Boolean =
     configuration.get[Boolean]("microservice.services.features.auditing.logout")
