38c62e45a907cd84a278c6ba674ced3bbe4a4a33
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2026-09-10 15:54:06 +0100

    DDCE-9176: Implement Service Navigation Accessibility Update for register-trust-trustee-frontend (#233)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,10 +2,10 @@ import sbt.*
 
 object AppDependencies {
 
-  private val playBootstrapVersion = "10.7.0"
+  private val playBootstrapVersion = "10.8.0"
 
   private lazy val compile = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "13.9.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "13.13.0",
     "uk.gov.hmrc" %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % playBootstrapVersion
